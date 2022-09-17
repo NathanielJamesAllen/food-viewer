56e4c48593e5dfe5d9ded2630292e6615203af2c
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2022-09-17 09:25:42 -0400

    Birthday

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {Typography} from '@mui/material'
 import SaveIcon from '@mui/icons-material/Save';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SaveIcon/>
-        <Typography>
-          James Rocks!
-        </Typography>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Layout></Layout>
+    
+    </>
   );
 }
 
---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+
+const Layout = () => {
+    return (
+        <Typography>I'm the Layout</Typography>
+
+    )
+}
+
+
+
+export default Layout
